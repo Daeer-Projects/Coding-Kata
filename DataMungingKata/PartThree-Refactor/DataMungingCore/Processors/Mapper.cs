7822c1cb99df8f4b867dfaa6574acd84f2fc90dc
MADDY\daeer <daeer73@gmail.com>
2019-05-29 20:29:50 +0100

    WIP: Figuring out the mapper template and methods.

--- DataMungingKata/PartThree-Refactor/DataMungingCore/Processors/Mapper.cs
+++ DataMungingKata/PartThree-Refactor/DataMungingCore/Processors/Mapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using DataMungingCoreV2.Interfaces;
@@ -11,6 +12,37 @@ namespace DataMungingCoreV2.Processors
         public static Task<IList<IDataType>> MapWork(Func<IList<IDataType>> mapResults)
         {
             return Task.Factory.StartNew(mapResults);
+        }
+
+        public static IList<IDataType> ExperimentalMapWork(string[] fileData,
+            Func<string, bool> checkItemRow,
+            Func<IList<IDataType>, string, IList<IDataType>> addDataItem)
+        {
+            var taskResults = MapDataToResults(fileData, checkItemRow, addDataItem);
+            return taskResults;
+        }
+
+        private static IList<IDataType> MapDataToResults(string[] fileData,
+            Func<string, bool> checkItemRow,
+            Func<IList<IDataType>, string, IList<IDataType>> addDataItem)
+        {
+            IList<IDataType> taskResults = new List<IDataType>();
+
+            return fileData.Aggregate(taskResults, (current, item) => AddData(item, current, checkItemRow, addDataItem));
+        }
+
+        private static IList<IDataType> AddData(string item, 
+            IList<IDataType> taskResults,
+            Func<string, bool> checkItemRow,
+            Func<IList<IDataType>, string, IList<IDataType>> addDataItem)
+        {
+            var results = taskResults;
+            if (checkItemRow(item))
+            {
+                results = addDataItem(results, item);
+            }
+
+            return results;
         }
     }
 }
--- DataMungingKata/PartThree-Refactor/FootballComponent/Processors/FootballMapper.cs
+++ DataMungingKata/PartThree-Refactor/FootballComponent/Processors/FootballMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using DataMungingCoreV2.Extensions;
 using DataMungingCoreV2.Interfaces;
@@ -7,6 +8,7 @@ using DataMungingCoreV2.Processors;
 using DataMungingCoreV2.Types;
 using FootballComponentV2.Constants;
 using FootballComponentV2.Extensions;
+using FootballComponentV2.Types;
 using FootballComponentV2.Validators;
 using Serilog;
 
@@ -37,34 +39,52 @@ namespace FootballComponentV2.Processors
             // We want to check the file has a header, an empty row, a footer and at least one row with data in it.
             if (!fileData.IsValid(new StringArrayValidator()).IsValid) throw new InvalidDataException("Invalid Data File.");
 
-            var results = await Mapper.MapWork(() =>
-            {
-                IList<IDataType> taskResults = new List<IDataType>();
-
-                foreach (var item in fileData)
-                {
-                    // Need to use the config to extract out the items...
-                    if (!item.Equals(FootballConstants.FootballHeader) && !item.Equals(FootballConstants.FootballDivider))
-                    {
-                        // So, not the header and not the divider.
-                        var footballData = item.ToFootball();
-                        if (footballData.IsValid)
-                        {
-                            _logger.Debug($"{GetType().Name} (MapAsync): Item valid: {item}.");
-                            taskResults.Add(new ContainingDataType { Data = footballData.Football });
-                        }
-                        else
-                        {
-                            _logger.Warning($"{GetType().Name} (MapAsync): Item not valid: {item}.");
-                        }
-                    }
-                }
-
-                return taskResults;
-            }).ConfigureAwait(false);
+            var results = await Mapper.MapWork(() => MapDataToResults(fileData)).ConfigureAwait(false);
+            //var results = await Mapper.ExperimentalMapWork(fileData, CheckItemRow, AddDataItem).ConfigureAwait(false);
 
             _logger.Information($"{GetType().Name} (MapAsync): Mapping complete.");
             return results;
+        }
+
+        private IList<IDataType> MapDataToResults(string[] fileData)
+        {
+            IList<IDataType> taskResults = new List<IDataType>();
+
+            return fileData.Aggregate(taskResults, (current, item) => AddData(item, current));
+        }
+
+        private IList<IDataType> AddData(string item, IList<IDataType> taskResults)
+        {
+            var results = taskResults;
+            if (CheckItemRow(item))
+            {
+                // So, not the header and not the divider.
+                results = AddDataItem(item, results);
+            }
+
+            return results;
+        }
+
+        private IList<IDataType> AddDataItem(string item, IList<IDataType> results)
+        {
+            var dataResults = results;
+            var footballData = item.ToFootball();
+            if (footballData.IsValid)
+            {
+                _logger.Debug($"{GetType().Name} (MapAsync): Item valid: {item}.");
+                dataResults.Add(new ContainingDataType {Data = footballData.Football});
+            }
+            else
+            {
+                _logger.Warning($"{GetType().Name} (MapAsync): Item not valid: {item}.");
+            }
+
+            return dataResults;
+        }
+
+        private static bool CheckItemRow(string item)
+        {
+            return !item.Equals(FootballConstants.FootballHeader) && !item.Equals(FootballConstants.FootballDivider);
         }
     }
 }
