07c2d2405c0c6f7631a700fc29deefac4f9cbcce
David Clark <david.clark2@capita.co.uk>
2020-02-26 13:53:17 +0000

    Added a new interview section.  First one for getting first non-repeated character.

---
+++ InterviewExperiments/Interview.Extensions/Extensions.Tests/FirstNonRepeatedCharacterTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Extensions.Tests
+{
+    public class FirstNonRepeatedCharacterTests
+    {
+        [Fact]
+        public void Test_get_first_character_with_empty_paragraph_returns_underscore()
+        {
+            // Arrange.
+            const char expected = '_';
+            var paragraph = string.Empty;
+
+            // Act.
+            var actual = paragraph.GetFirstNonRepeatedCharacter();
+
+            // Assert.
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("aaabcccdeeef", 'b')]
+        [InlineData("aaabcccdebeef", 'd')]
+        public void Test_get_first_character_with_valid_paragraph_returns_expected_character(string paragraph,
+            char expected)
+        {
+            // Arrange.
+            // Act.
+            var actual = paragraph.GetFirstNonRepeatedCharacter();
+
+            // Assert.
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ InterviewExperiments/Interview.Extensions/Extensions/FirstNonRepeatedCharacter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Concurrent;
+
+namespace Extensions
+{
+    public static class FirstNonRepeatedCharacter
+    {
+        public static char GetFirstNonRepeatedCharacter(this string paragraph)
+        {
+            var result = '_';
+
+            if (!string.IsNullOrWhiteSpace(paragraph))
+            {
+                var characters = new ConcurrentDictionary<char, int>();
+
+                for (var i = 0; i < paragraph.Length; i++)
+                {
+                    characters.AddOrUpdate(paragraph[i], 1,
+                        (key, existingValue) => existingValue + 1);
+                }
+                
+                foreach (var character in characters)
+                {
+                    if (character.Value == 1)
+                    {
+                        result = character.Key;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
